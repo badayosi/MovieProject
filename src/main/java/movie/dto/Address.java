5d16fee73e53a236063f567d0d5aa03fc1cf6b61
JaeJin Bae <bjj7425@naver.com>
2018-02-05 20:20:41 +0900

    dto 생성완료

---
+++ src/main/java/movie/dto/Address.java
@@ -0,0 +1,80 @@
+package movie.dto;
+
+public class Address {
+	String addrNo;
+	String sido;
+	String sigungu;
+	String doro;
+	int building1;
+	int building2;
+
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(String addrNo, String sido, String sigungu, String doro, int building1, int building2) {
+		super();
+		this.addrNo = addrNo;
+		this.sido = sido;
+		this.sigungu = sigungu;
+		this.doro = doro;
+		this.building1 = building1;
+		this.building2 = building2;
+	}
+
+	public String getAddrNo() {
+		return addrNo;
+	}
+
+	public void setAddrNo(String addrNo) {
+		this.addrNo = addrNo;
+	}
+
+	public String getSido() {
+		return sido;
+	}
+
+	public void setSido(String sido) {
+		this.sido = sido;
+	}
+
+	public String getSigungu() {
+		return sigungu;
+	}
+
+	public void setSigungu(String sigungu) {
+		this.sigungu = sigungu;
+	}
+
+	public String getDoro() {
+		return doro;
+	}
+
+	public void setDoro(String doro) {
+		this.doro = doro;
+	}
+
+	public int getBuilding1() {
+		return building1;
+	}
+
+	public void setBuilding1(int building1) {
+		this.building1 = building1;
+	}
+
+	public int getBuilding2() {
+		return building2;
+	}
+
+	public void setBuilding2(int building2) {
+		this.building2 = building2;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressDTO [addrNo=" + addrNo + ", sido=" + sido + ", sigungu=" + sigungu + ", doro=" + doro
+				+ ", building1=" + building1 + ", building2=" + building2 + "]";
+	}
+
+}
